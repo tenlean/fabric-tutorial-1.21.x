9603b7ab6c120a1e431a7517dfc5610262c890c6
Tenzin <tenzin.pepper@student.curtin.edu.au>
2026-06-02 17:05:10 +0800

    Added custom items

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 This is a starter Minecraft Fabric mod for version 1.21.
 
 ## What I'm Doing
-I am currently using this project to learn the fundamentals of Java Minecraft modding. I am following development tutorials to learn how to:
+
+I am currently using this project to learn the fundamentals of Java Minecraft modding. I am following development
+tutorials to learn how to:
+
 * Register custom items and blocks
 * Use data generation tools
 * Navigate the Fabric development environment
--- src/main/java/net/tenzinant/tutorialmod/TutorialMod.java
+++ src/main/java/net/tenzinant/tutorialmod/TutorialMod.java
@@ -2,15 +2,16 @@ package net.tenzinant.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tenzinant.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorialmod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-
-	}
+    @Override
+    public void onInitialize() {
+        ModItems.registerModItems();
+    }
 }
