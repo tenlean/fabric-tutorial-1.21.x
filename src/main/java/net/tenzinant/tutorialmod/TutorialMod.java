8ddf33407d2c93650a1244279cf7377808bd3e56
Tenzin <tenzin.pepper@student.curtin.edu.au>
2026-06-02 17:59:13 +0800

    Added custom blocks

--- src/main/java/net/tenzinant/tutorialmod/TutorialMod.java
+++ src/main/java/net/tenzinant/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.tenzinant.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tenzinant.tutorialmod.block.ModBlocks;
 import net.tenzinant.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
     }
 }
