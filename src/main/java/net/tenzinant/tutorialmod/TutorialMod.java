20831af5384d2aaa0fed4eea0974eaa3be49bf35
Tenzin <tenzin.pepper@student.curtin.edu.au>
2026-06-02 18:52:46 +0800

    Added item groups

--- src/main/java/net/tenzinant/tutorialmod/TutorialMod.java
+++ src/main/java/net/tenzinant/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.tenzinant.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.tenzinant.tutorialmod.block.ModBlocks;
+import net.tenzinant.tutorialmod.item.ModItemGroups;
 import net.tenzinant.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
     }
